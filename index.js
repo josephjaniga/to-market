6a11823033e7babaf96989f5b45f133b5b228349
josephjaniga <josephjaniga@gmail.com>
2015-11-09 23:47:54 -0500

    isolating logic into generic code paths

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 var GameServer = require('./lib/GameServer.class.js');
 
-module.exports = (()=>{
-    return new GameServer({port: process.env.PORT});
-})();
+module.exports = (options)=>{
+    return new GameServer({
+        port: process.env.PORT,
+        connect: options.connect,
+        disconnect: options.disconnect
+    });
+};

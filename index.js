30aed715ada13e523318733bb19f2aa050b59c95
josephjaniga <josephjaniga@gmail.com>
2015-11-10 00:32:35 -0500

    changed the API to include all of the classes

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 "use strict";
 
-var GameServer = require('./lib/GameServer.class.js');
+var Component = require('./lib/Component.class.js'),
+    Entity = require('./lib/Entity.class.js'),
+    Game = require('./lib/Game.class.js'),
+    GameServer = require('./lib/GameServer.class.js'),
+    Renderer = require('./lib/Renderer.class.js'),
+    Transform = require('./lib/Transform.class.js');
 
-module.exports = new GameServer();
+module.exports = {
+    Component,
+    Entity,
+    Game,
+    GameServer,
+    Renderer,
+    Transform
+};

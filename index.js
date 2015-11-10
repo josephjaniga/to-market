9a0ef1ab3df2f602c918d9508473a79d2a2aa0ed
Joe Janiga <josephjaniga@gmail.com>
2015-11-09 22:24:50 -0500

    back to commonjs require syntax

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 "use strict";
 
-import Component from "./lib/Component.class";
-import Entity from "./lib/Entity.class.js";
-import Game from "./lib/Game.class.js";
-import Transform from "./lib/Transform.class.js";
-import Renderer from "./lib/Renderer.class.js";
+var Game = require('Game.class');
 
 module.exports = (()=>{
     return new Game();

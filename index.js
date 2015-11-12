efca15952f8823cfc62653446660a47abb452039
Joe Janiga <josephjaniga@gmail.com>
2015-11-12 03:32:06 -0500

    added a class for user clients connected to the server, and better input handling for those users, restored the game to fully working prototype state but with nearly full ECS

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 "use strict";
 
-var Component = require('./lib/Component.class.js'),
+// the core library stuff
+var Client = require('./lib/Client.class.js'),
+    Component = require('./lib/Component.class.js'),
     Entity = require('./lib/Entity.class.js'),
     Game = require('./lib/Game.class.js'),
     GameServer = require('./lib/GameServer.class.js'),
+    InputState = require('./lib/InputState.class.js'),
     Renderer = require('./lib/Renderer.class.js'),
+    Time = require('./lib/Time.class.js'),
     Transform = require('./lib/Transform.class.js');
 
+var CharacterMotion = require('./lib/_CharacterMotion.class.js');
+
 module.exports = {
+    CharacterMotion,
+    Client,
     Component,
     Entity,
     Game,
     GameServer,
+    InputState,
     Renderer,
+    Time,
     Transform
 };

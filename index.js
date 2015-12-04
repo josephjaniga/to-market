abac6adf227e326b6b79de0453ace62e5ba7433b
joseph janiga <josephjaniga@gmail.com>
2015-12-03 23:07:43 -0500

    added OnCollisionEnter method to all entities and Components, fixed physics to properly check if rigidbody uses gravity, fixed shoot component bullet position, added destroy on collision component to bullets so they go away on hits

--- index.js
+++ index.js
@@ -17,12 +17,14 @@ var Client = require('./lib/Client.class.js'),
 // user implemented classes? this should go in the implementation of the game
 // these will be specific to each individual game but probably do not belong in a library
 var CharacterMotion = require('./lib/_CharacterMotion.class.js'),
+    DestroyOnCollision = require('./lib/_DestroyOnCollision.class.js'),
     Health = require('./lib/_Health.class.js'),
     Shoot = require('./lib/_Shoot.class.js');
 
 module.exports = ()=>{
     // User Implemented... should be split out eventually
     global.CharacterMotion = CharacterMotion;
+    global.DestroyOnCollision = DestroyOnCollision;
     global.Health = Health;
     global.Shoot = Shoot;
 
